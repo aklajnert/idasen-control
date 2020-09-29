e9afda595e2b95dbfec6aaf9abce40462804dd85
Andrzej Klajnert <github@aklajnert.pl>
2020-09-29 16:47:17 +0200

    Use idasen crate to show some basic behavior

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use idasen::Idasen;
+use std::thread;
+use std::time::Duration;
+
+pub fn main() -> Result<(), idasen::Error> {
+    let desk = Idasen::new()?;
+
+    println!("Desk addr: {}", desk.mac_addr);
+    println!("Position: {}", desk.position()?);
+
+    Ok(())
 }
